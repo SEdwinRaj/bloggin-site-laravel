3c1382716b54c2e5aab41107d2703e4fb041b029
SEdwinRaj <edwin.s.stud@gmail.com>
2023-12-10 19:10:25 +0530

    completed

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\blogUser;
 use App\Models\blog;
-use Illuminate\Support\Facades\Auth;
-use App\Models\User;
 
 class userController extends Controller
 {
@@ -24,7 +22,6 @@ class userController extends Controller
 
     public function signup(Request $request){
 
-        if(isset($request->email) && isset($request->password) && isset($request->username)){
             $data = $request->all();
             $user = new blogUser();
             $user->username = $data["username"];
@@ -46,11 +43,7 @@ class userController extends Controller
             //     return dd($data);
             // }
             // return "success";
-
-        }else{
-            return view('index')->with('error','something went wrong');
-        }
-    }
+            }
     public function signin(Request $request){
         // $request->validate([
         //     'name'=> 'required',
